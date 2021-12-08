81a7594730f1e1691ff78c7da949c58fa350b13e
fakeryanwu <fakeryanwu@tencent.com>
2021-12-08 14:21:37 +0800

    Changes

---
+++ session/hooks.go
@@ -0,0 +1,16 @@
+package session
+
+const (
+	BeforeQuery  = "BeforeQuery"
+	AfterQuery   = "AfterQuery"
+	BeforeUpdate = "BeforeUpdate"
+	AfterUpdate  = "AfterUpdate"
+	BeforeDelete = "BeforeDelete"
+	AfterDelete  = "AfterDelete"
+	BeforeInsert = "BeforeInsert"
+	AfterInsert  = "AfterInsert"
+)
+
+func (s *Session) CallMethod(method string, value interface{}) {
+
+}
